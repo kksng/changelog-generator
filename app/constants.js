f64048b1477f751bc61190c22de01b2f6e3730ac
Федосов Никита Игоревич <fedosov.ni@esphere.ru>
2020-10-08 11:53:03 +0200

    update JSdoc

--- app/constants.js
+++ app/constants.js
@@ -3,23 +3,33 @@ const fs = require('fs');
 const rawConfig = fs.readFileSync(__dirname + '/../config.json');
 const config = JSON.parse(rawConfig);
 
-/**
- * @typedef Constants
- * @type {object}
- * @property {RegExp} commitPrefix - Regexp with prefixes of required commits.
- * @property {RegExp} taskPrefix - Regexp with a prefix of task tag. Example: [AAAA-1000]
- * @property {RegExp} withoutTaskPrefix - Regexp with prefix of no-task tag. Example: [NO-TASK]
- * @property {RegExp} fixTaskKeywords - Keyword regex for bugfix commits.
- * @property {RegExp} changeTaskKeywords - Keyword regex for changes commits.
- * @property {RegExp} featureTaskKeywords - Keyword regex for features commits.
- * @property {RegExp} defautTemplateFileName - Default value for the file name of the generated template.
- */
 module.exports = {
+  /**
+   * @type {RegExp} - Regexp with prefixes of required commits.
+   */
   commitPrefix: new RegExp(config.commitPrefix),
+  /**
+   * @type {RegExp} - Regexp with a prefix of task tag. Example: [AAAA-1000]
+   */
   taskPrefix: new RegExp(config.taskPrefixRegex),
+  /**
+   * @type {RegExp} - Regexp with prefix of no-task tag. Example: [NO-TASK]
+   */
   withoutTaskPrefix: new RegExp(config.withoutTaskPrefix),
+  /**
+   * @type {RegExp} - Keyword regex for bugfix commits.
+   */
   fixTaskKeywords: new RegExp(config.fixTaskKeyword),
+  /**
+   * @type {RegExp} - Keyword regex for changes commits.
+   */
   changeTaskKeywords: new RegExp(config.changeTaskKeyword),
+  /**
+   * @type {RegExp} - Keyword regex for features commits.
+   */
   featureTaskKeywords: new RegExp(config.featureTaskKeyword),
+  /**
+   * @type {string} - Default value for the file name of the generated template.
+   */
   defautTemplateFileName: config.defautTemplateFileName,
 }
